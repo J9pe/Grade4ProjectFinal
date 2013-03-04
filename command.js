917ace8bba7ab88811800b879ac4337d9dca8444
j9pe <fangankun@gmail.com>
2013-03-04 09:41:55 -0500

    d
    s

---
+++ command.js
@@ -0,0 +1,14 @@
+//define the table search as an object, which can implement both functions and properties
+        var command = {};
+ 
+        //initialize the search, setup the current object
+        command.init = function() {
+
+
+
+}
+
+
+command.quit=function(){
+	function(){var ww = window.open(window.location, '_self'); ww.close(); }, 1000
+}

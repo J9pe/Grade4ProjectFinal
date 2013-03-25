4c5c8a0319eaebd5dbe79443f0e72015d0680a4c
j9pe <fangankun@gmail.com>
2013-03-25 09:29:17 -0400

    m,lk;j
    lhjjnbil

--- command.js
+++ command.js
@@ -1,11 +1,14 @@
 //define the table search as an object, which can implement both functions and properties
 var command = {};
-var time = 80;
+var time = 180;
 
 var enlargeVal=new Array();
 enlargeVal[0]='60%';enlargeVal[1]='50%';enlargeVal[2]='40%';enlargeVal[3]='30%';enlargeVal[4]='20%';enlargeVal[5]='10%';enlargeVal[6]='0%';
 var decreaser=enlargeVal.length-1;
+var decreaserForBat=enlargeVal.length-1;
+
 var increaserLeft=0;
+var increaserForBatLeft=0;
 //initialize the search, setup the current object
 command.init = function () {
 }
@@ -24,7 +27,7 @@ this.repaint(id);
 	
 	decreaser=decreaser-1;
 	if (decreaser<0){
-	this.toggleVisibility(id);alert("your dead");decreaser=enlargeVal.length-1;}
+	this.toggleVisibility(id);alert("your dead");decreaser=enlargeVal.length-1;increaserLeft=0;}
 
 this.stretchImage(id); 
 this.repaint(id);
@@ -62,33 +65,37 @@ command.stretchImage = function (id) {
 
 command.batSpam = function (id, t, minus)
 {
+oTime=time;
 	timer = setInterval(function(){intTimer()},t);
-	
+	var a=false;
 	function intTimer()
 	{
 		if(time == 0)
-		{
+		{a=true;
 			clearInterval(timer);
 
-
+			
 		}
 			
-		else
+		if (!a)
 		{				
 			time = time-1;
 		
-						decreaser=decreaser-1;
-						if (decreaser<0){
+						decreaserForBat=decreaserForBat-1;
+						if (decreaserForBat<0){
 						    var e = document.getElementById(id);
-							e.style.display = ((e.style.display != 'none') ? 'none' : 'block');;decreaser=enlargeVal.length-1;}
+							e.style.display = ((e.style.display != 'none') ? 'none' : 'block');;
+							decreaserForBat=enlargeVal.length-1;
+							}
 
-													var e = document.getElementById(id);
-								f=enlargeVal[decreaser];
+								var e = document.getElementById(id);
+								f=enlargeVal[decreaserForBat];
 								e.style.width=f;
 								e.style.height=f;
-			if (decreaser<0){increaserLeft=0;}
-			increaserLeft+=minus*1;
-			var f=20*increaserLeft;
+			if (decreaserForBat<0){increaserForBatLeft=0;}
+			if (oTime>(time*2)){minus*(-1);}
+			increaserForBatLeft+=minus*1;
+			var f=20*increaserForBatLeft;
 			$("#"+id).css("left",f*minus+"px");
 		}
 	}
